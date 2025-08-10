dd104b70764b3ff6f8733d7bad885c8861929cf5
Ram Kumar <rambiotech97@gmail.com>
2025-08-10 18:55:41 +0200

    Setup dbt and created a model

---
+++ dbt_modeling_project/models/stg/stg_raw_customers.sql
@@ -0,0 +1,10 @@
+
+
+With raw_customers as (
+    select * from JAFFLE.RAW_SCHEMA.RAW_CUSTOMERS
+)
+
+select * from raw_customers 
+
+
+
---
+++ dbt_modeling_project/models/stg/stg_raw_orders.sql
@@ -0,0 +1,5 @@
+With raw_orders as (
+    select * from JAFFLE.RAW_SCHEMA.RAW_ORDERS
+)
+
+select * from raw_orders
