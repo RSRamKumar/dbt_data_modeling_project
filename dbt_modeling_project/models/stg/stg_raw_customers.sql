5eea19cd981bc3daa613567bb8acdf287e9b7334
Ram Kumar <rambiotech97@gmail.com>
2025-08-10 19:04:27 +0200

    Updated raw_customers stg

--- dbt_modeling_project/models/stg/stg_raw_customers.sql
+++ dbt_modeling_project/models/stg/stg_raw_customers.sql
@@ -4,7 +4,11 @@ With raw_customers as (
     select * from JAFFLE.RAW_SCHEMA.RAW_CUSTOMERS
 )
 
-select * from raw_customers 
+select 
+    id as customer_id,
+    name as customer_name,
+    current_timestamp as load_timestamp
+ from raw_customers 
 
 
 
