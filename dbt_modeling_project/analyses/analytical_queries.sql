2b30cacfa5de26b913cc1f2f581fcc26f99016e3
Ram Kumar <rambiotech97@gmail.com>
2025-08-15 20:11:49 +0200

    Updated  analytical queries

--- dbt_modeling_project/analyses/analytical_queries.sql
+++ dbt_modeling_project/analyses/analytical_queries.sql
@@ -12,3 +12,17 @@ from  {{ ref('inter_products_enriched') }}
 )
 select product_id, product_name, product_type, NUMBER_OF_ITEMS_SOLD, rank from top_selling_product where rank <= 3
 
+-- Top 5 loyal customers
+select customer_id, customer_name, count(order_id) number_of_orders 
+from  {{ ref('inter_customer_orders_enriched') }}
+group by customer_id, customer_name 
+order by number_of_orders desc limit 5 
+ 
+
+
+-- Top 5 customers with highest sales
+select customer_id, customer_name, sum(order_total_amount) total_purchase_amount 
+from  {{ ref('inter_customer_orders_enriched') }}
+group by customer_id, customer_name 
+order by total_purchase_amount desc limit 5 
+ 
