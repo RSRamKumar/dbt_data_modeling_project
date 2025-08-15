6a7ce08ac7e28f161910fc42ef1f0d03251f5b1e
Ram Kumar <rambiotech97@gmail.com>
2025-08-15 20:12:24 +0200

    Updated  analytical queries

--- dbt_modeling_project/analyses/analytical_queries.sql
+++ dbt_modeling_project/analyses/analytical_queries.sql
@@ -2,27 +2,26 @@
 
 -- Top selling Product by revenue
 select * from {{ ref('inter_products_enriched') }} where total_revenue = (
-select max(total_revenue) from {{ ref('inter_products_enriched') }}
-)
+        select max(total_revenue) from {{ ref('inter_products_enriched') }}
+    )
 
 -- Top 3 selling product by NUMBER_OF_ITEMS_SOLD
 with top_selling_product as (
-select *, dense_rank() over(partition by product_type order by NUMBER_OF_ITEMS_SOLD desc) as rank 
-from  {{ ref('inter_products_enriched') }}
-)
+        select *, dense_rank() over (partition by product_type order by NUMBER_OF_ITEMS_SOLD desc) as rank
+        from {{ ref('inter_products_enriched') }}
+    )
 select product_id, product_name, product_type, NUMBER_OF_ITEMS_SOLD, rank from top_selling_product where rank <= 3
 
 -- Top 5 loyal customers
-select customer_id, customer_name, count(order_id) number_of_orders 
-from  {{ ref('inter_customer_orders_enriched') }}
-group by customer_id, customer_name 
-order by number_of_orders desc limit 5 
- 
+select customer_id, customer_name, count(order_id) number_of_orders
+from {{ ref('inter_customer_orders_enriched') }}
+group by customer_id, customer_name
+order by number_of_orders desc limit 5
+
 
 
 -- Top 5 customers with highest sales
-select customer_id, customer_name, sum(order_total_amount) total_purchase_amount 
-from  {{ ref('inter_customer_orders_enriched') }}
-group by customer_id, customer_name 
-order by total_purchase_amount desc limit 5 
- 
+select customer_id, customer_name, sum(order_total_amount) total_purchase_amount
+from {{ ref('inter_customer_orders_enriched') }}
+group by customer_id, customer_name
+order by total_purchase_amount desc limit 5
