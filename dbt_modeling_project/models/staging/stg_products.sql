e45e804f2b2e24f8bc9ed7c4a3d57ee8b5402146
Ram Kumar <rambiotech97@gmail.com>
2025-08-16 17:24:40 +0200

    Updated stg_products

--- dbt_modeling_project/models/staging/stg_products.sql
+++ dbt_modeling_project/models/staging/stg_products.sql
@@ -1,33 +1,26 @@
-with raw_products
-    as (
-
-        select
-            'SKU', 'NAME', 'TYPE', 'PRICE', 'DESCRIPTION', 'DBT_SCD_ID', 'DBT_UPDATED_AT', 'DBT_VALID_FROM', 'DBT_VALID_TO'
-
-
-        from {{ ref('scd_products') }} -- jaffle.dev_schema.scd_products
-
-    )
+with raw_products as (
+    select
+        sku,
+        name,
+        type,
+        price,
+        description,
+        DBT_SCD_ID,
+        DBT_UPDATED_AT,
+        DBT_VALID_FROM,
+        DBT_VALID_TO
+    from {{ ref('scd_products') }}
+)
 
 select
-
     sku as product_id,
-
-    name
-    as product_name,
-
-    type
-    as product_type,
-
+    name as product_name,
+    type as product_type,
     price as product_price,
-
-    description
-    as product_description,
-
+    description as product_description,
     DBT_SCD_ID,
     DBT_UPDATED_AT,
     DBT_VALID_FROM,
     DBT_VALID_TO,
     case when DBT_VALID_TO is null then true else false end as is_current
-
 from raw_products
