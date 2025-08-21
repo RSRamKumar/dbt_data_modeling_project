05132530387b767e58754cd5e80a4d90222e8069
Ram Kumar <rambiotech97@gmail.com>
2025-08-21 17:51:36 +0200

    Linted the sql

--- dbt_modeling_project/analyses/analytical_queries.sql
+++ dbt_modeling_project/analyses/analytical_queries.sql
@@ -1,67 +1,54 @@
 
 -- Top selling Product by revenue
-select dp.product_name, dense_rank() over (order by  sum(fcp.item_total) desc) as rank
-from {{ ref('fct_order_products') }}  fcp
-join {{ ref('dim_products') }}  dp 
-using(product_sk)
-group by dp.product_name 
-qualify rank <= 3
+select dp.product_name, dense_rank() over (order by sum(fcp.item_total) desc) as rank
+from {{ ref('fct_order_products') }} fcp
+    join {{ ref('dim_products') }} dp
+    using (product_sk)
+group by dp.product_name
+    qualify rank <= 3
 
 
 
 -- Top 3 selling product by number of items sold
 select dp.product_name, dense_rank() over (order by sum(quantity) desc) as rank
-from fct_order_products  fcp
-join dim_products dp 
-using(product_sk)
+from fct_order_products fcp
+    join dim_products dp
+    using (product_sk)
 group by dp.product_name
-qualify rank <= 3
+    qualify rank <= 3
 
 
 
 -- Top 5 loyal customers (by number of orders)
-select dc.customer_id, dc.customer_name, 
-       count(distinct fo.order_id) as number_of_orders,
-       dense_rank() over(order by count(distinct fo.order_id) desc) as rank
-from fct_orders fo  
-join dim_customers dc using (customer_sk)
+select dc.customer_id, dc.customer_name,
+    count(distinct fo.order_id) as number_of_orders,
+    dense_rank() over (order by count(distinct fo.order_id) desc) as rank
+from fct_orders fo
+    join dim_customers dc using (customer_sk)
 group by dc.customer_id, dc.customer_name
-qualify rank <= 5
+    qualify rank <= 5
 
 -- Another method directly from mart
-select customer_name, lifetime_orders_count as number_of_orders, 
-dense_rank() over(order by lifetime_orders_count desc) as rank
-from {{ ref('customer_orders_summary') }} 
-qualify rank <= 5
- 
+select customer_name, lifetime_orders_count as number_of_orders,
+    dense_rank() over (order by lifetime_orders_count desc) as rank
+from {{ ref('customer_orders_summary') }}
+    qualify rank <= 5
+
 
 -- Top 5 customers with highest sales
-select customer_name, sum(order_total_amount) as total_order_amount, dense_rank() over (order by sum(order_total_amount) desc ) as rank
-from fct_orders fo 
-join dim_customers dc 
-using(customer_sk)
+select customer_name, sum(order_total_amount) as total_order_amount,
+    dense_rank() over (order by sum(order_total_amount) desc) as rank
+from fct_orders fo
+    join dim_customers dc
+    using (customer_sk)
 group by customer_name
-qualify rank <= 5
+    qualify rank <= 5
 
 
 select * from {{ ref('customer_orders_summary') }} limit 5
 
 
 
--- Debug Pathways
-select order_id, count(*) 
-from fct_orders
-group by order_id
-having count(*) > 1;
 
 
 
--- Check if two customers share the same name
-select customer_name, count(distinct customer_id) as custs
-from dim_customers
-group by customer_name
-having count(distinct customer_id) > 1;
-
-
-
- 
