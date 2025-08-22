f0679a7acc559f50faad935505a378f26128978e
Ram Kumar <rambiotech97@gmail.com>
2025-08-22 18:17:12 +0200

    Updated store sales summary

--- dbt_modeling_project/models/gold/3.mart/store_sales_summary.sql
+++ dbt_modeling_project/models/gold/3.mart/store_sales_summary.sql
@@ -1,4 +1,6 @@
--- store_sales_summary
+-- store_sales_summary 
+-- 1 row = 1 store and metrics
+
 
 with orders as (
     select
@@ -8,16 +10,20 @@ with orders as (
         order_total_amount,
         total_items_count
     from {{ ref('fct_orders') }}
+),
+dim_stores as (
+    select store_sk, store_id, location_name from {{ ref('dim_stores') }}
 )
 
 select 
     o.store_sk,
+    ds.location_name,
 
     -- Core metrics
     sum(o.order_total_amount)      as total_sales_amount,
     sum(o.total_items_count)       as total_items_sold,
     count(distinct o.order_id)     as total_orders_count,
-    count(distinct o.customer_id)  as unique_customers_count,
+    count(distinct o.customer_sk)  as unique_customers_count,
 
     -- KPI metrics
     round(sum(o.order_total_amount) / nullif(count(distinct o.order_id), 0), 2) as avg_order_value,
@@ -26,5 +32,5 @@ select
     current_timestamp() as load_timestamp 
 
 from orders o 
-group by store_sk 
-
+join dim_stores ds using(store_sk)
+group by o.store_sk, ds.location_name
