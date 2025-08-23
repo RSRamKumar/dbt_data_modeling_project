50f3a3b1105ae1a236809b05a09bb7d7e1b8024d
Ram Kumar <rambiotech97@gmail.com>
2025-08-23 18:50:43 +0200

    Updated product sales mart

--- dbt_modeling_project/models/gold/3.mart/product_sales_summary.sql
+++ dbt_modeling_project/models/gold/3.mart/product_sales_summary.sql
@@ -5,8 +5,7 @@ with order_products as (
         select
             product_sk,
             quantity,
-            store_sk,
-            product_price
+            store_sk
         from  {{ ref('fct_order_products') }}
     ),
     dim_products as (
@@ -17,8 +16,8 @@ with order_products as (
         op.product_sk,
         dp.product_name, 
         sum(op.quantity) as total_quantity_sold, 
-        sum(op.quantity) * op.product_price as product_revenue
+        sum(op.quantity * op.product_price) as product_revenue
     from order_products op 
     join dim_products dp 
     on op.product_sk = dp.product_sk
-    group by  op.product_sk, dp.product_name, op.product_price
+    group by  op.product_sk, dp.product_name 
