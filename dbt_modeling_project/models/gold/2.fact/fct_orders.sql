e5ae8918f006d031e33abd7e014169d9d3bf87b8
Ram Kumar <rambiotech97@gmail.com>
2025-09-06 18:36:27 +0200

    Updated fct orders as incremental

--- dbt_modeling_project/models/gold/2.fact/fct_orders.sql
+++ dbt_modeling_project/models/gold/2.fact/fct_orders.sql
@@ -1,5 +1,12 @@
 -- fct_orders.sql one row per order
 -- Fact table with surrogate keys for customer, product
+
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'order_id',
+    on_schema_change = 'append_new_columns'
+) }}
+
 with enriched_orders as (
         select
             order_id,
@@ -12,16 +19,32 @@ with enriched_orders as (
             total_items_count,
             store_id
         from {{ ref('inter_orders_enriched') }}
+            {% if is_incremental() %}
+        -- only pull rows newer than the max order_date already loaded
+        where order_date > (
+                select max(d.date_actual)
+                from {{ this }} f
+                    join {{ ref('dim_dates') }} d
+                    on f.date_sk = d.date_sk
+            )
+            {% endif %}
     ),
+
     dim_customers as (
-        select customer_sk, customer_id from {{ ref('dim_customers') }}
+        select customer_sk, customer_id
+        from {{ ref('dim_customers') }}
     ),
+
     dim_dates as (
-        select date_actual, date_sk from {{ ref('dim_dates') }}
+        select date_actual, date_sk
+        from {{ ref('dim_dates') }}
     ),
+
     dim_stores as (
-        select store_sk, store_id from {{ ref('dim_stores') }}
+        select store_sk, store_id
+        from {{ ref('dim_stores') }}
     )
+
 select
     eo.order_id,
     dd.date_sk,
@@ -33,11 +56,13 @@ select
     eo.total_items_count,
     ds.store_sk,
     current_timestamp() as load_timestamp
-
 from enriched_orders eo
-    join dim_dates dd on eo.order_date = dd.date_actual
-    join dim_customers dc on eo.customer_id = dc.customer_id
-    join dim_stores ds on eo.store_id = ds.store_id
+    join dim_dates dd
+    on eo.order_date = dd.date_actual
+    join dim_customers dc
+    on eo.customer_id = dc.customer_id
+    join dim_stores ds
+    on eo.store_id = ds.store_id
 
 
    -- and eo.order_date between dc.effective_date and coalesce(dc.end_date, current_date)
