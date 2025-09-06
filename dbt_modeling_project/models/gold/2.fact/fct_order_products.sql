2fbca3b55d364cb56b5aef60ea61330ad1ceebec
Ram Kumar <rambiotech97@gmail.com>
2025-09-06 18:45:01 +0200

    Updated fct order_products as incremental

--- dbt_modeling_project/models/gold/2.fact/fct_order_products.sql
+++ dbt_modeling_project/models/gold/2.fact/fct_order_products.sql
@@ -1,62 +1,84 @@
 
--- fct_order_items.sql 
+-- fct_order_items.sql
 -- grain =  1 row per (order_id, product_id)  -- one record per unique product within an order
 
 
-  with order_items as (
-          select order_id, product_id from {{ ref('stg_order_items') }}
-      ),
-      orders as (
-          select order_id, order_date, customer_id, store_id from {{ ref('stg_orders') }}
-      ),
-      dim_dates as (
-          select date_actual, date_sk from {{ ref('dim_dates') }}
-      ),
-      dim_products as (
-          select product_sk, product_id, product_price,   from {{ ref('dim_products') }}
-      ),
-      dim_customers as (
-          select customer_sk, customer_id  from {{ ref('dim_customers') }}
-      ),
-      dim_stores as (
-        select store_sk, store_id from {{ ref('dim_stores') }}
+{{ config(
+    materialized = 'incremental',
+    unique_key = ['order_id', 'product_sk'],
+    on_schema_change = 'append_new_columns'
+) }}
+
+with order_items as (
+        select order_id, product_id
+        from {{ ref('stg_order_items') }}
+    ),
+
+    orders as (
+        select order_id, order_date, customer_id, store_id
+        from {{ ref('stg_orders') }}
+            {% if is_incremental() %}
+        -- only load new orders not already in the fact table
+        where order_date > (
+                select max(d.date_actual)
+                from {{ this }} f
+                    join {{ ref('dim_dates') }} d
+                    on f.date_sk = d.date_sk
+            )
+            {% endif %}
+    ),
+
+    dim_dates as (
+        select date_actual, date_sk
+        from {{ ref('dim_dates') }}
+    ),
+
+    dim_products as (
+        select product_sk, product_id, product_price
+        from {{ ref('dim_products') }}
+    ),
+
+    dim_customers as (
+        select customer_sk, customer_id
+        from {{ ref('dim_customers') }}
+    ),
+
+    dim_stores as (
+        select store_sk, store_id
+        from {{ ref('dim_stores') }}
     )
 
-
-
-  select o.order_id, dd.date_sk, dc.customer_sk, dp.product_sk, ds.store_sk, count(*) as quantity,
-      dp.product_price, count(*) * dp.product_price as item_total
-
-  from order_items i
-      join orders o
-      on i.order_id = o.order_id
-      join dim_customers dc on
-      o.customer_id = dc.customer_id
-    --  and o.order_date between dc.effective_date and coalesce(dc.end_date, current_date)
-      join dim_dates dd
-      on o.order_date = dd.date_actual
-      join dim_products dp
-      on i.product_id = dp.product_id
-      -- and o.order_date between dp.effective_date and coalesce(dp.end_date, current_date)
-      join dim_stores ds 
-      on o.store_id = ds.store_id
-
- -- If order date is not between product effective date (date of insertion of products), query produces no results
-
-
-  group by o.order_id,
-      dd.date_sk,
-      dc.customer_sk,
-      dp.product_sk,
-      ds.store_sk,
-      dp.product_price
-
-   
-
+select
+    o.order_id,
+    dd.date_sk,
+    dc.customer_sk,
+    dp.product_sk,
+    ds.store_sk,
+    count(*) as quantity,
+    dp.product_price,
+    count(*) * dp.product_price as item_total,
+    current_timestamp() as load_timestamp
+from order_items i
+    join orders o
+    on i.order_id = o.order_id
+    join dim_customers dc
+    on o.customer_id = dc.customer_id
+    join dim_dates dd
+    on o.order_date = dd.date_actual
+    join dim_products dp
+    on i.product_id = dp.product_id
+    join dim_stores ds
+    on o.store_id = ds.store_id
+group by
+    o.order_id,
+    dd.date_sk,
+    dc.customer_sk,
+    dp.product_sk,
+    ds.store_sk,
+    dp.product_price
 
 
 
- 
 --For quantity
 --If in the future stg_order_items includes a quantity field (e.g., a customer buys 3 coffees in one order item line)
---SUM(COALESCE(i.quantity, 1)) as quantity  
+--SUM(COALESCE(i.quantity, 1)) as quantity
